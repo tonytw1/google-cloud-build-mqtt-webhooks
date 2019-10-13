cffdeead6a0c5bb751f5b409f9c73e1e316283ea
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-10-13 16:21:13 +0100

    Compile.

--- app/model/model.scala
+++ app/model/model.scala
@@ -3,4 +3,4 @@ package model
 case class Push(message: Message, subscription: String)
 
 case class Message(messageId: String, publishTime: String, attributes: Attributes, data: String)
-case class Attributes(buildId: String, status: String, )
+case class Attributes(buildId: String, status: String)
