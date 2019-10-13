e022ae13e7b001b9718191683436d20bf82cd571
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-10-13 10:51:17 +0100

    Blindly push webhooks into MQTT channel.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,15 @@
 package controllers
 
+import mqtt.MQTTService
 import play.api.Logger
 import play.api.libs.json.{JsValue, Json}
-import play.api.mvc.{BodyParsers, Action, Controller}
+import play.api.mvc.{Action, BodyParsers, Controller}
 
 import scala.concurrent.Future
 
 object Application extends Controller {
+
+  val mqttService = MQTTService
 
   def home = Action.async { request =>
     Future.successful(Ok(Json.toJson("Ok")))
@@ -15,6 +18,7 @@ object Application extends Controller {
   def webhook = Action.async(BodyParsers.parse.json) { request =>
     val message: JsValue = request.body
     Logger.info("Received webhook: " + message)
+    mqttService.publish(message.toString())
     Future.successful(Ok(Json.toJson("Thanks!")))
   }
 
---
+++ app/mqtt/MQTTService.scala
@@ -0,0 +1,55 @@
+package mqtt
+
+import java.io.{File, FileInputStream}
+import java.security.KeyStore
+import java.security.cert.{CertificateFactory, X509Certificate}
+import javax.net.ssl.{SSLContext, TrustManagerFactory}
+
+import org.fusesource.mqtt.client.{MQTT, QoS}
+import play.api.Play.current
+import play.api.{Logger, Play}
+
+object MQTTService {
+
+  private val host = Play.configuration.getString("mqtt.host").get
+  private val port = Play.configuration.getInt("mqtt.port").get
+  private val topic = Play.configuration.getString("mqtt.topic").get
+  private val caCert = Play.configuration.getString("mqtt.tls.cacert")
+
+  def publish(message: String) = {
+    Logger.info("Publishing to mqtt topic " + host + ":" + port + " / " + topic + ": " + message)
+    val connection = getClient().blockingConnection
+    connection.connect
+    connection.publish(topic, message.getBytes, QoS.AT_MOST_ONCE, false)
+    connection.disconnect
+  }
+
+  private def getClient(): MQTT = {
+
+    def sslContext(certPath: String): SSLContext = {
+      val cf = CertificateFactory.getInstance("X.509")
+      val asStream = new FileInputStream(new File(certPath))
+      val caCert = cf.generateCertificate(asStream).asInstanceOf[X509Certificate]
+      val tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
+      val ks = KeyStore.getInstance(KeyStore.getDefaultType)
+      ks.load(null)
+      ks.setCertificateEntry("caCert", caCert)
+      tmf.init(ks)
+      val sslContext = SSLContext.getInstance("TLS")
+      sslContext.init(null, tmf.getTrustManagers, null)
+      sslContext
+    }
+
+    caCert.fold {
+      val mqtt = new MQTT()
+      mqtt.setHost(host, port)
+      mqtt
+    } { c =>
+      val mqtt = new MQTT()
+      mqtt.setHost("tls://" + host + ":" + port)
+      mqtt.setSslContext(sslContext(c))
+      mqtt
+    }
+  }
+
+}
